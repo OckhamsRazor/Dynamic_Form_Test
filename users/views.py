709d35f4d536059b6deed8ebccf9e92e66adffff
Lang-Chi Yu <ockhamsrazor@YLC-GT680.(none)>
2014-10-27 22:55:19 +0800

    add app wiki

--- users/views.py
+++ users/views.py
@@ -13,14 +13,16 @@ from django.contrib.auth.decorators import login_required
 
 from celery import shared_task
 
-import Web
+from Web import settings
 import utils.consts as consts
-from .models import MyUser, HASH_KEY_LENGTH, DEFAULT_PROFILE_PIC
+from .models import MyUser, MyFile, \
+    HASH_KEY_LENGTH, DEFAULT_PROFILE_PIC
 from .forms import SignUpForm
 from utils.decorators import post_only_view, post_only_json
-from utils.views import handle_file_upload
 from utils.utils import generate_user, existence_checking, \
     random_string, confirmation_mail_content
+from utils.views import handle_file_upload
+
 
 @post_only_json
 def login_request(request):
@@ -112,6 +114,12 @@ def new_account(request):
 
 def settings(request):
     context = {}
+    if request.user.is_authenticated():
+        if request.user.profile_pic.file == "":
+            context["profile_pic"] = DEFAULT_PROFILE_PIC
+        else:
+            context["profile_pic"] = request.user.profile_pic.url()
+
     return render(request, "settings/settings.html", context)
 
 def admin(request):
@@ -157,7 +165,7 @@ def email_activation(request, code):
 @shared_task
 @login_required
 @post_only_json
-def upload(request):
+def upload_profile_pic(request):
     result = consts.FAILED
     user = request.user
     new_profile_pic_url = user.profile_pic.file
@@ -173,20 +181,21 @@ def upload(request):
             request.FILES['profile_pic'].name,
         )
         new_profile_pic_url = path.join(
-            Web.settings.MEDIA_URL,
+            settings.MEDIA_URL,
             new_profile_pic_url_short
         )
         new_profile_pic_path = path.join(
-            Web.settings.MEDIA_ROOT,
+            settings.MEDIA_ROOT,
             new_profile_pic_url_short
         )
         handle_file_upload(
             request, request.FILES['profile_pic'], new_profile_pic_path
         )
 
-        user.profile_pic.file = new_profile_pic_url_short
-        user.profile_pics.files.append(user.profile_pic)
+        if user.profile_pic.file != "":
+            user.profile_pics.files.append(user.profile_pic)
         user.profile_pics.file_no += 1
+        user.profile_pic = MyFile(file=new_profile_pic_url_short)
         user.save()
         result = consts.SUCCESSFUL
     except Exception as e:
@@ -199,12 +208,12 @@ def upload(request):
 
 @login_required
 @post_only_json
-def crop_avatar(request):
+def crop_profile_pic(request):
     result = consts.FAILED
     user = request.user
     try:
         img_path = path.join(
-            Web.settings.MEDIA_ROOT,
+            settings.MEDIA_ROOT,
             user.profile_pic.file.lstrip("/")
         )
         img = Image.open(img_path)
@@ -217,7 +226,7 @@ def crop_avatar(request):
         )
         img.crop(box).save(img_path)
         result = consts.SUCCESSFUL
-    except Exception as e: # should do security check here!
+    except Exception as e:
         print traceback.format_exc()
 
     return {'result': result}
@@ -225,15 +234,23 @@ def crop_avatar(request):
 @login_required
 def show_profile_pics(request):
     user = request.user
+    profile_pic = user.profile_pic
     profile_pics = user.profile_pics.files
 
-    context = {"profile_pics": profile_pics}
+    context = {
+        "profile_pic": profile_pic,
+        "profile_pic_candidates": profile_pics
+    }
     return render(request, "settings/profile_pics.html", context)
 
 @login_required
-def remove_profile_pic(request):
+def change_profile_pic(request):
+    pass
+
+@login_required
+def delete_profile_pics(request):
     pass
     # request.user.profile_pic = DEFAULT_PROFILE_PIC
-    # dir_path = path.join(Web.settings.MEDIA_ROOT, "img", request.user.username)
+    # dir_path = path.join(settings.MEDIA_ROOT, "img", request.user.username)
     # if path.exists(dir_path):
         # rmtree(dir_path)
---
+++ wiki/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
+from . import views
+
+urlpatterns = patterns('',
+)
